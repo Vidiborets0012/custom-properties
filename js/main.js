26f9556e7671f32f95b02610fca1fcf67009b499
Vidiborets Viktor <vidiboretsv@gmail.com>
2025-04-12 17:50:15 +0300

    add js to the page__js block

---
+++ js/main.js
@@ -0,0 +1,7 @@
+let range = document.querySelector('.range');
+let divider = document.querySelector('body');
+
+range.addEventListener('input', () => {
+  let output = Math.abs(range.value);
+  divider.style.setProperty('--divider', output);
+});
